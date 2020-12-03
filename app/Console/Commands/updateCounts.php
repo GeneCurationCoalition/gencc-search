e5bb9d8e2258212f8bf7b57f71e8842db0c04eee
Scott G <sgoehringer@users.noreply.github.com>
2020-12-03 13:29:09 -0500

    WIP

--- app/Console/Commands/updateCounts.php
+++ app/Console/Commands/updateCounts.php
@@ -108,7 +108,7 @@ class updateCounts extends Command
 
                 if (isset($val->disease)) {
                     if(isset($list['count_unique_diseases'][$val->disease->curie])) {
-                        $list['count_unique_diseases'][$val->disease->curie]    = $list[$val->classification->slug][$val->disease->curie] + 1;
+                        $list['count_unique_diseases'][$val->disease->curie]    = $list[$val->disease->slug][$val->disease->curie] + 1;
                     } else {
                         $list['count_unique_diseases'][$val->disease->curie]    = 1;
                     }
