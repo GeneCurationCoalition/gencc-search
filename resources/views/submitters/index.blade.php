6ac5d7147abd72bfeda211b7abd3bdca2630d1e4
Scott G <sgoehringer@users.noreply.github.com>
2020-12-03 14:27:48 -0500

    WIP

--- resources/views/submitters/index.blade.php
+++ resources/views/submitters/index.blade.php
@@ -12,9 +12,9 @@
 </div>
 @endsection
 @section('content')
-<div class="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
+<div class="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-2 xl:grid-cols-3 lg:max-w-none">
     @forelse ($submitters as $submitter)
-      <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+      <div class="flex flex-col rounded-lg shadow-lg border border-gray-400 overflow-hidden">
         <div class="flex-shrink-0">
           <a href="{{ route('submitter-show', $submitter->uuid) }}" class="block">
           <img class="h-48 w-full object-cover" src="{{ $submitter->path_logo }}" alt="">
@@ -38,22 +38,22 @@
             <p class="mt-2 text-sm leading-5 font-medium text-gray-500">
                 Submission Stats
             </p>
-          <div class="mt-1 mb-3 flex items-center">
+          <div class="mt-1 mb-3 flex items-center -ml-4 -mr-4">
               <div class="grid grid-cols-9 gap-1 w-full">
                     <div class="col-span-3 bg-gray-300 border-white-100 border-solid border-2 rounded-full py-1 px-1">
-                        <div class='grid grid-cols-3 gap-1'>
+                        <div class='grid grid-cols-3 gap-1/2'>
                             {!! $submitter->displayCurationCountPill($submitter->curations_definitive, "definitive", route('submitter-show', $submitter->uuid)) !!}
                             {!! $submitter->displayCurationCountPill($submitter->curations_strong, "strong", route('submitter-show', $submitter->uuid)) !!}
                             {!! $submitter->displayCurationCountPill($submitter->curations_moderate, "moderate", route('submitter-show', $submitter->uuid)) !!}
                         </div>
                     </div>
                     <div class="col-span-1 bg-gray-300 border-white-100 border-solid border-2 rounded-full py-1 px-1">
-                        <div class='grid grid-cols-1 gap-1'>
+                        <div class='grid grid-cols-1 gap-1/2'>
                             {!! $submitter->displayCurationCountPill($submitter->curations_supportive, "supportive", route('submitter-show', $submitter->uuid)) !!}
                          </div>
                     </div>
                     <div class="col-span-5 bg-gray-300 border-white-100 border-solid border-2 rounded-full py-1 px-1">
-                        <div class='grid grid-cols-5 gap-1'>
+                        <div class='grid grid-cols-5 gap-1/2'>
                             {!! $submitter->displayCurationCountPill($submitter->curations_limited, "limited", route('submitter-show', $submitter->uuid)) !!}
                             {!! $submitter->displayCurationCountPill($submitter->curations_disputed, "disputed", route('submitter-show', $submitter->uuid)) !!}
                             {!! $submitter->displayCurationCountPill($submitter->curations_refuted, "refuted", route('submitter-show', $submitter->uuid)) !!}
