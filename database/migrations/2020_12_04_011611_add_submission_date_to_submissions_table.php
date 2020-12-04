132280f6c6585bda0f756ac2c940a688d5d8e118
Scott G <sgoehringer@users.noreply.github.com>
2020-12-03 20:18:42 -0500

    WIP

---
+++ database/migrations/2020_12_04_011611_add_submission_date_to_submissions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSubmissionDateToSubmissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('submissions', function (Blueprint $table) {
+            //
+            $table->date('submitted_as_run_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('submissions', function (Blueprint $table) {
+            //
+        });
+    }
+}
