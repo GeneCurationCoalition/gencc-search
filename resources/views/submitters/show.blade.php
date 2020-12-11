7329df1b7b1e29dcbc3b7631af2ef955660426ba
Scott G <sgoehringer@users.noreply.github.com>
2020-12-11 13:00:57 -0500

    Submitter text

--- resources/views/submitters/show.blade.php
+++ resources/views/submitters/show.blade.php
@@ -11,22 +11,27 @@
       {{-- <hr class="mt-4 border" /> --}}
     </div>
     <div class="col-span-12 xl:col-span-1 py-3">
-      <p>Per in ridens elaboraret. Ne vix amet maluisset, sed te eros harum expetenda, in eos atqui adversarium. Nibh scaevola signiferumque est in. Vim at impetus consequuntur, tota inimicus expetendis et sed. Eius accusata pro eu.</p>
+      <p>{!! $submitter->text_description !!}</p>
       @if($submitter->website)
       <hr class="mt-3 mb-3 border" />
       <strong>Website</strong>
-      <div class="mb-1"><a href="{{ $submitter->website }}" target="_website" >{{ $submitter->website }} <i class="fas fa-external-link-alt"></i></a></div>
+      <div class="mb-1">
+        <a href="{{ $submitter->website }}" class="text-blue-700 underline" target="_blank" >{{ $submitter->website }} <i class="fas fa-external-link-alt"></i></a></div>
       @endif
       <hr class="mt-3 mb-3 border" />
       <strong>Personnel</strong>
-      <div class="mb-2">John XXXXXX, Coordinator<br />
-          Phone: 800-XXX-XXXXXX<br />
-          Email: XXXXX.XXXXXXX@XXXXXXX.com
+      <div class="mb-2">
+        {!! $submitter->text_contact !!}
       </div>
+        @isset($submitter->text_assertions)
         <hr class="mt-3 mb-3 border" />
         <strong>Assertion Criteria</strong>
-        <div class="mb-1">website link here.... <i class="fas fa-external-link-alt"></i></div>
-        <div class="mb-1">website link here.... <i class="fas fa-external-link-alt"></i></div>
+        <div class="mb-2">
+          @foreach (explode('<br>' ,$submitter->text_assertions) as $item)
+              <div class="truncate"><a class="text-blue-700 underline" href="{{ $item }}">{{ $item }} <i class="fas fa-external-link-alt"></i></a></div>
+          @endforeach
+        </div>
+        @endisset
 
     </div>
     <div class="col-span-12 xl:col-span-1 bg-gray-200 p-3">
@@ -48,11 +53,11 @@
           &nbsp;
         </a>
         <a href="{{ route('genes') }}?curations_definitive=1&curations_strong=1&curations_moderate=1&curations_limited=1&curations_disputed=1&curations_refuted=1&curations_animal=1&curations_noknown=1&{{ $item->href }}=0">
-            <span class="font-bold">{{ $item->displayStatChartBarPercentSubmitter($submitter, $item->href, 'count') }}</span> Curations
+            <span class="font-bold">{{ $item->displayStatChartBarPercentSubmitter($submitter, $item->href, 'count') }}</span> Submissions
           </a>
         @else
         <a class="pt-1 inline-block" href="{{ route('genes') }}?curations_definitive=1&curations_strong=1&curations_moderate=1&curations_limited=1&curations_disputed=1&curations_refuted=1&curations_animal=1&curations_noknown=1&{{ $item->href }}=0">
-            <span class="font-bold">{{ $item->displayStatChartBarPercentSubmitter($submitter, $item->href, 'count') }} </span> Curations
+            <span class="font-bold">{{ $item->displayStatChartBarPercentSubmitter($submitter, $item->href, 'count') }} </span> Submissions
           </a>
         @endif
       </div>
@@ -64,65 +69,7 @@
 </div>
 <div class="grid grid-cols-12 mt-10 gap-0">
     <div class="col-span-12">
-
-
             @livewire('submitter.listing-of-submissions', ['submitter' => $submitter])
-
-
-              {{-- @foreach($submitter->submissions as $item)
-                  <div class="border-t border-t-gray-200 border-t-solid pt-0 mt-2 text-sm">
-                      <div class="grid grid-cols-10 gap-2 my-3 ">
-
-                          <div class="col-span-1 -mt-1"><a href="{{ route('submission-show', $item->uuid) }}">{!!  $item->displayCurationLabelPill($item->classification) !!}</a></div>
-                          <div class="col-span-3  ml-0 mr-3">
-                            <div class="flex">
-                              <div class="flex-initial mr-1 leading-tight">
-                                <i class="fas fa-dna text-gray-400"></i>
-                              </div>
-                              <div class="flex-initial leading-tight">
-                                @if($item->gene)
-                                <a class="list-text-label" href="{{ route('gene-show', $item->gene->uuid) }}"> {{ $item->gene->title }}</a>
-                                <div class="text-sm text-gray-600">{{ $item->gene->curie }}</div>
-                                @endif
-                              </div>
-                              <div class="flex-initial ml-4 mr-1">
-                                <i class="far fa-disease text-gray-400"></i>
-                              </div>
-                              <div class="flex-initial break-words leading-tight">
-                                <span class="list-text-label">{{ $submitter->displayMondoDisease($item->diseases)->first()->title }}</span>
-                                <div class="text-sm text-gray-600">{{ $submitter->displayMondoDisease($item->diseases)->first()->curie }}</div>
-                                <div class="mt-1 text-sm text-gray-600"> Submitted as: {{ $item->disease->curie }}</div>
-                              </div>
-                            </div>
-
-                          </div>
-                          <div class="col-span-1"><span class="list-text-label"> {{ $item->inheritance->abbreviation }} <i class="far fa-question-circle text-gray-400" title="{{ $item->inheritance->title }} Mode Of Inheritance " data-toggle="tooltip" data-placement="top" \></i></div>
-                          <div class="col-span-1"><i class="far fa-calendar text-gray-400"></i> <span class="list-text-label">{{ Carbon\Carbon::parse($item->submitted_as_date)->format('m/d/Y') }}</span><div class="text-sm text-gray-600 ml-4">Evaluated</div>
-                        <div class="text-sm mt-3 text-gray-600 ml-4 font-semibold leading-snug">{{ $item->submitted_run_date->format('m/d/Y') }}<div class=" font-normal">Submitted</div></div>
-                      </div>
-                          <div class="col-span-4">
-
-                            <a href="{{ route('submitter-show', $item->submitter->uuid) }}" class=""><i class="far fa-building text-gray-400"></i> <span class="list-text-label">{{ $item->submitter->title }}</span></a>
-                            <div class="ml-4 pt-1 text-sm flex-inline-list">
-                              <ul>
-                                @if($item->submitted_as_public_report_url)
-                              <li><a target="assertion_criteria_url" href="{{ $item->submitted_as_public_report_url }}" class="text-blue-600">Public Report <i class="fas fa-external-link-alt"></i></a></li>
-                                @endif
-                                @if($item->submitted_as_assertion_criteria_url)
-                              <li><a target="assertion_criteria_url" href="{{ $item->submitted_as_assertion_criteria_url }}" class="text-blue-600">Assertion Criteria <i class="fas fa-external-link-alt"></i></a></li>
-                                @endif
-                              <li><a class="text-blue-600" href="{{ route('submission-show', $item->uuid) }}">More Details <i class="far fa-arrow-alt-circle-right"></i></a></li>
-                              </ul>
-                            </div>
-                            @if($item->submitted_as_notes)
-                            <div class="ml-4 pt-1 text-sm text-gray-800">Evidence: {!! \Illuminate\Support\Str::limit($item->submitted_as_notes, 100, $end='... <a class="text-gray-600 underline" href="'. route('submission-show', $item->uuid) .'">Read more <i class="far fa-arrow-alt-circle-right"></i></a>') ?? ''!!}</div>
-                            @endif
-
-                          </div>
-                          <div class="col-span-1 text-right"></div>
-                      </div>
-                  </div>
-              @endforeach --}}
       </div>
 </div>
 
