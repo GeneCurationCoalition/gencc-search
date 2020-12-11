d10a20f085e94324d647a755e36e5926cb91bb3d
Scott G <sgoehringer@users.noreply.github.com>
2020-12-11 13:56:34 -0500

    Tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,16 @@
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	@livewireStyles
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y9ZHWJMZNK"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-Y9ZHWJMZNK');
+	</script>
+
 </head>
 
 
