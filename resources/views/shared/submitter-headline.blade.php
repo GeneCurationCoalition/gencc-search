bc9c8a622be84dfb200afff70c67b28993982704
Scott G <sgoehringer@users.noreply.github.com>
2020-12-11 21:05:42 -0500

    faq

--- resources/views/shared/submitter-headline.blade.php
+++ resources/views/shared/submitter-headline.blade.php
@@ -2,7 +2,7 @@
       <div class="col-span-10 text-white"><h1 class=" truncate">{{ $submitter->title }}</h1></div>
       <div class="col-span-2 pt-5">
         <div class="text-right text-md"><a href="{{ route("submitters") }}" class="rounded-full py-1 px-5 text-center border-2 border-solid border-blue-900 text-blue-700 bg-white whitespace-no-wrap hover:bg-blue-200"><i class="far fa-arrow-alt-circle-left"></i> Return to list</a></div>
-                <div class="text-right"><a class="px-3" target="_blank" href="https://thegencc.org/resources/help.html#subitter-show"><i class="fas fa-question-circle"></i> Help</a></div>
+                <div class="text-right"><a class="px-3" target="_blank" href="https://thegencc.org/faq.html#subitter-show"><i class="fas fa-question-circle"></i> Help</a></div>
 
       </div>
   </div>
