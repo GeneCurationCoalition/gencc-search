d454f15667d49b94eb616a4b89a7d0e8cf4111a2
Scott G <sgoehringer@users.noreply.github.com>
2020-12-17 11:09:51 -0500

    tailwind classes

---
+++ resources/views/vendor/pagination/sample.blade.php
@@ -0,0 +1,8 @@
+{{-- NOTE - KEEP THIS... USING AS A PLACE TO STORE CSS THATS NEEDED --}}
+
+<div class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"></div>
+<div class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"></div>
+<div class="flex items-center justify-between"></div>
+<div class="flex justify-between flex-1 sm:hidden"></div>
+<div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between"></div>
+<div class="w-5 h-5"></div>
