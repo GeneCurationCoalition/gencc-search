15dae81c50d3ba792b37edae47b5ad472b8a9378
Scott G <sgoehringer@users.noreply.github.com>
2020-12-17 11:08:54 -0500

    mobility update

--- resources/views/livewire/submitter/listing-of-submissions.blade.php
+++ resources/views/livewire/submitter/listing-of-submissions.blade.php
@@ -10,7 +10,7 @@
 
       </div>
 
-      <div class="mt-1 flex rounded-md shadow-sm">
+      <div class="mt-1 flex flex-col lg:flex-row rounded-md shadow-sm">
           <span class="inline-flex shadow items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
             Filters:
           </span>
