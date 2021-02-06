1eb60ac08f899aa9f9afd2b2a53d2d10dfbaf86c
Scott G <sgoehringer@users.noreply.github.com>
2021-02-06 11:39:21 -0500

    Hide unused feature

--- resources/views/livewire/dashboard/process-submissions.blade.php
+++ resources/views/livewire/dashboard/process-submissions.blade.php
@@ -4,10 +4,10 @@
         <i class="animate-spin fas fa-spinner"></i> Processing submissions...
 
     </span> --}}
-    <button wire:click="start" class=" border-green-800 border-2 bg-green-100 hover:bg-green-300 font-bold mx-auto p-1 px-3 rounded focus:outline-none focus:shadow-outline float-right" type="button"><i class="fas fa-running"></i> Process Submissions</button>
+    {{-- <button wire:click="start" class=" border-green-800 border-2 bg-green-100 hover:bg-green-300 font-bold mx-auto p-1 px-3 rounded focus:outline-none focus:shadow-outline float-right" type="button"><i class="fas fa-running"></i> Process Submissions</button>
     @if($messages)
     <div>
        <pre>{{ $messages }}</pre>
     </div>
-    @endif
+    @endif --}}
 </div>
