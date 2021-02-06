d360f39a116f7a4fc06d4111846ced66eb0c9412
Scott G <sgoehringer@users.noreply.github.com>
2021-02-06 11:36:13 -0500

    Correction for total submission counts

--- app/Http/Controllers/StatController.php
+++ app/Http/Controllers/StatController.php
@@ -21,7 +21,7 @@ class StatController extends Controller
         $genesCount = Gene::with('submissions')->has('submissions')->count();
         $diseasesCount = Disease::with('submissions')->has('submissions')->count();
         $submitters_with_submissions = Submitter::has('submissions');
-        $submissionsCount = Submission::count();
+        $submissionsCount = Submission::where('status', '=', 1)->count();
         $classifications = Classification::with('submissions')->with('submissions')->get();
         $submitters = Submitter::where('status', 1)->paginate(25);
         $page_meta['seo']['title'] = "GenCC Submission Statistics";
