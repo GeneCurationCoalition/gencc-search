85d1242522a51baec8015c4ccd489ad57e062f71
Phillip Weller <pweller1@geisinger.edu>
2025-04-28 11:47:29 -0400

    Missed file from last commit

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 // Publish endpoint
-Route::post('/publish', '\PublishController@process');
+Route::post('/publish', 'PublishController@process');
