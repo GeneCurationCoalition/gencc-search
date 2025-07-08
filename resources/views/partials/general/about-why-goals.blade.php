318ccfb3bc3b9d7c02e44b540979aa613cece1c7
Kyle Ferriter <kferrite@broadinstitute.org>
2025-07-08 14:56:09 -0400

    Move about why/goals content to a partial

---
+++ resources/views/partials/general/about-why-goals.blade.php
@@ -0,0 +1,22 @@
+    <div class="mt-0">
+        <!-- https://v1.tailwindcss.com/docs/upgrading-to-v1#17-double-check-your-default-line-height -->
+        <h2 class="my-3">Why is the GenCC needed?</h2>
+        <p class="mb-4 text-gray-900 leading-normal">
+            Several groups and resources provide information that pertains to the validity of gene-disease relationships; however, the standards and terminologies to define the evidence base for a gene’s role in disease are still evolving, and the community is in need of trusted and harmonized sources that define the level of evidence for a gene’s role in disease. To tackle this issue, the Gene Curation Coalition (GenCC) was formed.
+        </p>
+        <p class="mb-4 text-gray-900 leading-normal">
+            The Gene Curation Coalition brings together groups engaged in the evaluation of gene-disease validity with a willingness to share data publicly, to develop consistent terminology for gene curation activities, and to facilitate the consistent assessment of genes that have been reported in association with disease.
+        </p>
+    </div>
+
+    <div class="col-12 mt-10"><hr /></div>
+
+    <div class="mt-10">
+        <h2 class="my-3">The goals of the GenCC are as follows:</h2>
+        <ul class="space-y-2 text-gray-900 leading-normal ml-6 pl-4" style="list-style-type: disc !important; list-style-position: outside !important;">
+            <li>Clarify the overlap between gene curation efforts</li>
+            <li>Understand the aims, processes, information used, classification systems, and users of the different curation efforts</li>
+            <li>Develop consistent terminology for validity assessment as well as inheritance, allelic requirement, and mechanism of disease</li>
+            <li>Collaborate on gene curation projects</li>
+        </ul>
+    </div>
