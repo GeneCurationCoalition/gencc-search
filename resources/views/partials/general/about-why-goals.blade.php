05731f54c3c2253540a12c47e89e56d249f8e11f
Kyle Ferriter <kferrite@broadinstitute.org>
2025-07-08 15:01:10 -0400

    Update /about to match the more recent 'about'  sections on thegencc/faq

--- resources/views/partials/general/about-why-goals.blade.php
+++ resources/views/partials/general/about-why-goals.blade.php
@@ -1,10 +1,10 @@
     <div class="mt-0">
         <!-- https://v1.tailwindcss.com/docs/upgrading-to-v1#17-double-check-your-default-line-height -->
-        <h2 class="my-3">Why is the GenCC needed?</h2>
-        <p class="mb-4 text-gray-900 leading-normal">
+        <h2 class="my-3">What is the GenCC and why is it needed?</h2>
+        <p class="mb-4">
             Several groups and resources provide information that pertains to the validity of gene-disease relationships; however, the standards and terminologies to define the evidence base for a gene’s role in disease are still evolving, and the community is in need of trusted and harmonized sources that define the level of evidence for a gene’s role in disease. To tackle this issue, the Gene Curation Coalition (GenCC) was formed.
         </p>
-        <p class="mb-4 text-gray-900 leading-normal">
+        <p class="mb-4">
             The Gene Curation Coalition brings together groups engaged in the evaluation of gene-disease validity with a willingness to share data publicly, to develop consistent terminology for gene curation activities, and to facilitate the consistent assessment of genes that have been reported in association with disease.
         </p>
     </div>
@@ -12,11 +12,16 @@
     <div class="col-12 mt-10"><hr /></div>
 
     <div class="mt-10">
-        <h2 class="my-3">The goals of the GenCC are as follows:</h2>
-        <ul class="space-y-2 text-gray-900 leading-normal ml-6 pl-4" style="list-style-type: disc !important; list-style-position: outside !important;">
-            <li>Clarify the overlap between gene curation efforts</li>
-            <li>Understand the aims, processes, information used, classification systems, and users of the different curation efforts</li>
+        <h2 class="my-3">What are the goals of the GenCC?</h2>
+
+        <p class="mb-4">
+            The GenCC is working to harmonize approaches to ensure gene-level resources are comparable and interoperable. This allows groups to work together most effectively and to provide consistent and useful resources for the community. The goals of the GenCC are as follows:
+        </p>
+
+        <ul class="space-y-2 ml-6 pl-4" style="list-style-type: disc !important; list-style-position: outside !important;">
+            <li>Understand the aims, processes, information used, classification systems, and users of the different gene curation efforts</li>
             <li>Develop consistent terminology for validity assessment as well as inheritance, allelic requirement, and mechanism of disease</li>
-            <li>Collaborate on gene curation projects</li>
+            <li>Develop the GenCC Database, a shared resource to bring together gene-level resources</li>
+            <li>Collaborate to harmonize gene-disease curations</li>
         </ul>
     </div>
